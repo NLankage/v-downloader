eb0eea913d851878616d2b20340bca871a5796b7
Nelaka Lankage <nelakalankage@gmail.com>
2026-02-13 20:19:10 +0530

    monetag 1.3

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10605998
+    "zoneId": 10606575
 }
 self.lary = ""
 importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
