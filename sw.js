157f8dd6ef7ebbdf1a906ea1beb1e02cbefac0ae
Nelaka Lankage <nelakalankage@gmail.com>
2026-02-13 17:58:46 +0530

    monetag 1.0

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 self.options = {
-    "domain": "5gvci.com",
-    "zoneId": 10605971
+    "domain": "3nbf4.com",
+    "zoneId": 10605998
 }
 self.lary = ""
-importScripts('https://5gvci.com/act/files/service-worker.min.js?r=sw')
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
